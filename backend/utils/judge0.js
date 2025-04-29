8384dd865c42ac5fcc49ecfc115dc9015a084fec
YeahmanS <yamansingh85@gmail.com>
2025-04-29 22:19:26 +0530

    we are done with boilerplate code

---
+++ backend/utils/judge0.js
@@ -0,0 +1,11 @@
+const getJudge0LanguageId = (language) =>{
+    const languageMap = {
+        "PYTHON" : 71,
+        "JAVA" : 62,
+        "JAVASCRIPT" : 63
+    }
+
+    return languageMap[language.toUpperCase()]
+}
+
+export {getJudge0LanguageId}
