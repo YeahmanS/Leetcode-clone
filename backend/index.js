86ef4335d4c1d8d5d19f593fe8956e9d02ab3b3f
YeahmanS <yamansingh85@gmail.com>
2025-04-29 21:37:08 +0530

    added some routes and did judge0

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+
+
+import authRouter from "./routes/auth.route.js";
+import problemRouter from "./routes/problem.route.js";
 
 dotenv.config();
 
@@ -13,6 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/v1/auth",authRouter)
+app.use("api/v1/problems",problemRouter)
 
 app.listen(PORT,()=>{
     console.log("Listing on PORT :",PORT)
